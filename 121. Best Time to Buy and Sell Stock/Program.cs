d309b4ca6cecca3cb7cb743577981b44b9e4fabc
vvavilov <vvavilov@users.noreply.github.com>
2022-03-31 14:48:22 +0400

    Solve some more

---
+++ 121. Best Time to Buy and Sell Stock/Program.cs
@@ -0,0 +1,35 @@
+﻿public class Solution {
+    public int MaxProfit(int[] prices) {
+        /*
+            iterate 0 -> n with i
+            set buy, sell
+            calculate profit
+            val < buy -> update buy
+            val > sell -> update sell
+            recalculate profit
+            
+            1 3 11 2 1 15
+        
+        */
+        
+        var max = Int32.MinValue;
+        
+        var sell = 0;
+        var buy = 0;
+        
+        for(int i = 0; i < prices.Length; i++) {
+            if(prices[i] > prices[sell]) {
+                sell = i;
+            }
+            if(prices[i] < prices[buy]) {
+                buy = i;
+                sell = i;
+            }
+            max = Math.Max(max, prices[sell] - prices[buy]);
+        }
+        
+        return max;
+        
+        
+    }
+}
---
+++ 448. Find All Numbers Disappeared in an Array/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Solution {
+    public IList<int> FindDisappearedNumbers(int[] nums) {
+        for(int i = 0; i < nums.Length; i++) {
+            var n = Math.Abs(nums[i]);
+            nums[n - 1] = 0 - Math.Abs(nums[n - 1]);
+        }
+        
+        var result = new List<int>();
+        
+        for(int i = 0; i < nums.Length; i++) {
+            if(nums[i] > 0) {
+                result.Add(i+1);
+            }
+        }
+        
+        return result;
+    }
+}
