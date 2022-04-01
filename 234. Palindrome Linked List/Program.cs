e72abe6c003863727f96910a074f03a15fca2ce8
vvavilov <vvavilov@users.noreply.github.com>
2022-04-01 16:18:24 +0400

    Solve a couple of simple ones

---
+++ 234. Palindrome Linked List/Program.cs
@@ -0,0 +1,39 @@
+﻿public class Solution {
+    public bool IsPalindrome(ListNode head) {
+        if(head == null || head.next == null) { return true;}
+        var secondHead = FindSecondHalf(head);
+        var secondRev = RevertList(secondHead);
+        while(secondRev != null) {
+            if (head.val != secondRev.val) {
+                return false;
+            }
+            secondRev = secondRev.next;
+            head = head.next;
+            
+        }
+        return true;
+    }
+    
+    private ListNode FindSecondHalf(ListNode head) {
+        var slow = head;
+        var fast = head;
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        return slow;
+    }
+    
+    private ListNode RevertList(ListNode list) {
+        ListNode prev = null;
+        while(list != null) {
+            var next = list.next;
+            list.next = prev;
+            prev = list;
+            list = next;
+        }
+        
+        return prev;
+    }
+}
