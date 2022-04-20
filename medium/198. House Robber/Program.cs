5ad6a0e63d21d58057ae2be9703ea69b790d0dd9
vvavilov <vvavilov@users.noreply.github.com>
2022-04-20 22:13:57 +0400

    solve one mid and organize by compexity

---
+++ medium/198. House Robber/Program.cs
@@ -0,0 +1,34 @@
+﻿public class Solution {
+//     1 2 3 4 5 6
+//
+        
+        //     6 -> 6
+    //     56 -> max(5,6);
+    //    456 -> max(sum(4,f(6), f(5))
+    //   3456 -> max(sum(3,f(56)), sum(4,f(6))
+    //  23456 -> max(sum(2, f(456)), sum(3, f(56)))
+    // 123456 -> max(sum(1, f(3456)), sum(2, f(456)),
+//         f(n) = {
+//                  6 -> n[6]
+//                  5 -> n[5]
+                    // 1-4 -> max(n[n] + n[n+2], n[n+1])
+//         
+            
+            
+    public int Rob(int[] nums) {
+        if(nums.Length == 1) {
+            return nums[0];
+        }
+        
+        var steps = new int[nums.Length];
+        steps[0] = nums[0];
+        steps[1] = Math.Max(nums[1], nums[0]);
+        for(int i = 2; i < nums.Length; i++) {
+            var robCurrent = nums[i] + steps[i-2];
+            var doNotRobCurrent = steps[i-1];
+            steps[i] = Math.Max(robCurrent, doNotRobCurrent);
+        }
+        
+        return steps[nums.Length - 1];
+    }
+}
