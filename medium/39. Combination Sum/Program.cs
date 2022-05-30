1f7320728c755b1958f817ba593ea11abb0d53f0
vvavilov <vvavilov@users.noreply.github.com>
2022-05-30 17:30:39 +0400

    Some backtarcking

---
+++ medium/39. Combination Sum/Program.cs
@@ -0,0 +1,32 @@
+﻿public class Solution {
+    public IList<IList<int>> CombinationSum(int[] candidates, int target) {
+        List<IList<int>> result = new();
+        Array.Sort(candidates);
+        CombinationSum(candidates, target, 0, result, new LinkedList<int>());
+        
+        return result;
+    }
+    
+    private void CombinationSum(int[] candidates, int target, int index, IList<IList<int>> result, LinkedList<int> cur) {
+        if(target == 0) {
+            result.Add(new List<int>(cur));
+            return;
+        }
+        
+        if(target < 0) {
+            return;
+        }
+        
+        for(int i = index; i < candidates.Length; i++) {
+            var curItem = candidates[i];
+            
+            if(i > index && curItem == candidates[i-1]) {
+                continue;
+            }
+            
+            cur.AddLast(curItem);
+            CombinationSum(candidates, target - curItem, i, result, cur);
+            cur.RemoveLast();
+        }
+    }
+}
---
+++ medium/90. Subsets II/Program.cs
@@ -0,0 +1,53 @@
+﻿public class Solution {
+    public IList<IList<int>> SubsetsWithDup(int[] nums) {
+        
+        
+        List<IList<int>> result = new();
+        Array.Sort(nums);
+        Subsets(nums, 0, new LinkedList<int>(), result);
+        
+        return result;
+    }
+    
+    private void Subsets(int[] nums, int index, LinkedList<int> current, List<IList<int>> result) {
+        result.Add(new List<int> (current));
+        
+        for(int i = index; i < nums.Length; i++) {
+            if(i > index && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            current.AddLast(nums[i]);
+            Subsets(nums, i + 1, current, result);
+            current.RemoveLast();
+        }
+    }
+    
+}
+
+public class SolutionWithoutLoop {
+    public IList<IList<int>> SubsetsWithDup(int[] nums) {
+        List<IList<int>> result = new();
+        
+        Subsets(nums, 0, new LinkedList<int>(), result);
+        
+        return result;
+    }
+    
+    private void Subsets(int[] nums, int index, LinkedList<int> current, IList<IList<int>> result) {
+        if(index == nums.Length) {
+            result.Add(new List<int>(current));
+        }
+        
+        current.AddLast(nums[index]);
+        Subsets(nums, index + 1, current, result);
+        
+        current.RemoveLast();
+        
+        if(index == 0 || nums[index] != result.Last?.Value) {
+            Subsets(nums, index + 1, current, result);
+        }
+
+        
+        
+    }
+}
