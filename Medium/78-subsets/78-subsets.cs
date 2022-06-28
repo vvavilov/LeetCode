5c2c2e2b44d17056139e50c933e1169c1d6104a2
vvavilov <vvavilov@users.noreply.github.com>
2022-06-28 13:08:59 +0400

    Time: 231 ms (19.69%), Space: 41.7 MB (22.86%) - LeetHub

---
+++ Medium/78-subsets/78-subsets.cs
@@ -0,0 +1,39 @@
+
+
+public class Solution {
+    public IList<IList<int>> Subsets(int[] nums) {
+        List<IList<int>> result = new();
+        Subsets(nums, 0,  new LinkedList<int>(), result);
+        return result;
+    }
+    
+    private void Subsets(int[] nums, int index, LinkedList<int> current, IList<IList<int>> result) {
+        if(index == nums.Length) {
+            result.Add(new List<int>(current));
+            return;
+        }
+
+        current.AddLast(nums[index]);
+        Subsets(nums, index + 1, current, result);
+        current.RemoveLast();
+        Subsets(nums, index + 1, current, result);
+    }
+}
+
+public class SolutionWithLoop {
+    public IList<IList<int>> Subsets(int[] nums) {
+        List<IList<int>> result = new();
+        Subsets(nums, 0,  new LinkedList<int>(), result);
+        return result;
+    }
+    
+    private void Subsets(int[] nums, int index, LinkedList<int> current, IList<IList<int>> result) {
+        result.Add(new List<int>(current));
+        
+        for(int i = index; i < nums.Length; i++) {
+            current.AddLast(nums[i]);
+            Subsets(nums, i + 1, current, result);
+            current.RemoveLast();
+        }
+    }
+}
