d839e14fb42c5b54ed4fd7138ad4aa4fb494906d
vvavilov <vvavilov@users.noreply.github.com>
2022-06-28 13:06:21 +0400

    Time: 240 ms (25.97%), Space: 41.4 MB (100.00%) - LeetHub

---
+++ Medium/40-combination-sum-ii/40-combination-sum-ii.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public IList<IList<int>> CombinationSum2(int[] candidates, int target) {
+        Array.Sort(candidates);
+    
+        List<IList<int>> result = new();
+        Backtrack(candidates, target, new List<int>(), result, 0);
+        return result;
+    }
+    
+    private void Backtrack(int[] candidates, int target, List<int> current, IList<IList<int>> result, int pos) {
+        if(target == 0) {
+            result.Add(new List<int>(current));
+            return;
+        }
+        
+        if(target < 0) {
+            return;
+        }
+                
+        for(int i = pos; i < candidates.Length; i++) {
+            if(i > pos && candidates[i] == candidates[i-1]) {
+                continue;
+            }
+            var val = candidates[i];
+            current.Add(val);
+            Backtrack(candidates, target - val, current, result, i + 1);
+            current.RemoveAt(current.Count - 1);
+        }
+    }
+}
