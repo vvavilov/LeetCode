6762edb133246af1c97bb47b29a985d779360af8
vvavilov <vvavilov@users.noreply.github.com>
2022-06-28 11:18:13 +0400

    Time: 207 ms (64.24%), Space: 43.3 MB (35.54%) - LeetHub

---
+++ Easy/1-two-sum/1-two-sum.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        var dict = new Dictionary<int, int>();
+        
+        for(int i = 0; i < nums.Length; i++) {
+            if(dict.ContainsKey(target - nums[i])) {
+                return new int[] { dict[target-nums[i]], i};
+            }
+            
+            dict[nums[i]] = i;
+        }
+        
+        return new int[0];
+    }
+}
