21bdcdca0b24e3e4d5deb782b848366bf3a9a995
vvavilov <vvavilov@users.noreply.github.com>
2022-07-10 14:50:54 +0400

    Time: 137 ms (28.14%), Space: 36.6 MB (66.54%) - LeetHub

--- Medium/213-house-robber-ii/213-house-robber-ii.cs
+++ Medium/213-house-robber-ii/213-house-robber-ii.cs
@@ -1,31 +1,28 @@
 public class Solution {
     public int Rob(int[] nums) {
+        if(nums.Length == 0) {
+            return 0;
+        }
+        
         if(nums.Length == 1) {
             return nums[0];
         }
-        /*
-        1 2 3 4 5
-        each -> rob\do not rob with money result
-        each - will have next state based on prev
-        1 is decided to rob - we cannot rob 5
-        1 is decided to skip - we can rob 5
-        */
-        var robFirstPlan = PlanRobbery(nums, 0, nums.Length - 2 );
-        var doNotRobFirst = PlanRobbery(nums, 1, nums.Length - 1);
-        return Math.Max(robFirstPlan, doNotRobFirst);
+        
+        var takeFirst = Maximize(nums, 0, nums.Length - 2);
+        var skipFirst = Maximize(nums, 1, nums.Length - 1);
+        return Math.Max(takeFirst, skipFirst);
     }
     
-    public int PlanRobbery(int[] nums, int firstStep, int lastStep) {
-        var rob = nums[firstStep];
-        var doNotRob = 0;
+    private int Maximize(int[] nums, int start, int end) {
+        var lastTakenAcc = nums[start];
+        var lastSkippedAcc = 0;
         
-        for(int i = firstStep + 1; i <= lastStep; i++) {
-            var tryToRob = doNotRob + nums[i];
-            doNotRob = Math.Max(doNotRob, rob);
-            rob = tryToRob;
+        for(int i = start + 1; i <= end; i++) {
+            var currentTakenAcc = lastSkippedAcc + nums[i];
+            lastSkippedAcc = Math.Max(lastSkippedAcc, lastTakenAcc);
+            lastTakenAcc = currentTakenAcc;
         }
         
-        return Math.Max(doNotRob, rob);
-        
+        return Math.Max(lastTakenAcc, lastSkippedAcc);
     }
 }
