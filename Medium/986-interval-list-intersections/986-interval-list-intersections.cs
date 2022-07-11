6a47946fa13098a4ea43a4323a3bf2a3f7a6fd47
vvavilov <vvavilov@users.noreply.github.com>
2022-07-11 23:28:04 +0400

    Time: 278 ms (25.00%), Space: 46.3 MB (25.96%) - LeetHub

--- Medium/986-interval-list-intersections/986-interval-list-intersections.cs
+++ Medium/986-interval-list-intersections/986-interval-list-intersections.cs
@@ -8,10 +8,12 @@ public class Solution {
         while(firstPos < firstList.Length && secondPos < secondList.Length) {
             var firstInterval = firstList[firstPos];
             var secondInterval = secondList[secondPos];
-            var intersection = GetIntersection(firstInterval, secondInterval);
             
-            if(intersection != null) {
-                result.Add(intersection);
+            var left = Math.Max(firstInterval[0], secondInterval[0]);
+            var right = Math.Min(firstInterval[1], secondInterval[1]);
+            
+            if(left <= right) {
+                result.Add(new int[] {left, right});
             }
             
             if(firstInterval[1] >= secondInterval[1]) {
@@ -25,22 +27,5 @@ public class Solution {
         }
         
         return result.ToArray();
-    }
-    
-    public int[] GetIntersection(int[] x, int[] y) {
-        var hasIntersection = IntersectsWith(x, y) || IntersectsWith(y, x);
-        
-        if(!hasIntersection) {
-            return null;
-        }
-        
-        return new int[] {
-            Math.Max(x[0], y[0]),
-            Math.Min(x[1], y[1])
-        };
-    }
-    
-    private bool IntersectsWith(int[] source, int[] target) {
-        return source[0] >= target[0] && source[0] <= target[1];
     }
 }
