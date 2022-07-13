8eb09b25dee7b23333d85282956488bab6935c22
vvavilov <vvavilov@users.noreply.github.com>
2022-07-13 14:07:46 +0400

    Time: 142 ms (29.02%), Space: 37.7 MB (43.65%) - LeetHub

--- Medium/162-find-peak-element/162-find-peak-element.cs
+++ Medium/162-find-peak-element/162-find-peak-element.cs
@@ -1,20 +1,22 @@
 public class Solution {
     public int FindPeakElement(int[] nums) {
+        if(nums.Length == 0) {
+            return -1;
+        }
+
         var left = 0;
         var right = nums.Length - 1;
         
         while(left < right) {
-            var mid = (int)Math.Ceiling((right - left) / 2d + left);
-            var isIncreasing = nums[mid] > nums[mid - 1];
+            var mid = (right - left) / 2 + left;
             
-            if(isIncreasing) {
-                left = mid;
+            if(nums[mid] > nums[mid+1]) {
+                right = mid;
             } else {
-                right = mid - 1;
+                left = mid + 1;
             }
         }
         
         return left;
     }
-    
 }
