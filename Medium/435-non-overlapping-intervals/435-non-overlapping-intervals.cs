5fe66b648615b3efabe459c079bfd8016b0bca02
vvavilov <vvavilov@users.noreply.github.com>
2022-07-13 11:04:17 +0400

    Time: 655 ms (36.13%), Space: 63.5 MB (89.57%) - LeetHub

--- Medium/435-non-overlapping-intervals/435-non-overlapping-intervals.cs
+++ Medium/435-non-overlapping-intervals/435-non-overlapping-intervals.cs
@@ -1,16 +1,14 @@
 public class Solution {
     public int EraseOverlapIntervals(int[][] intervals) {
-        intervals = intervals.OrderBy(x => x[0]).ToArray();
-        
         if(intervals.Length == 0) {
             return 0;
         }
         
+        var sorted = intervals.OrderBy(x => x[0]);
         var count = 0;
-        var prevEnd = intervals[0][1];
+        var prevEnd = sorted.First()[1];
         
-        for(int i = 1; i < intervals.Length; i++) {
-            var cur = intervals[i];
+        foreach(var cur in sorted.Skip(1)) {
             var areOverlaps = cur[0] < prevEnd;
             
             if(areOverlaps) {
