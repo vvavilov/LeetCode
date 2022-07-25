ca7dde471959ededf2483c50ce74349c8c2325a4
vvavilov <vvavilov@users.noreply.github.com>
2022-07-25 15:48:55 +0400

    Time: 148 ms (63.61%), Space: 39.5 MB (64.73%) - LeetHub

--- Medium/45-jump-game-ii/45-jump-game-ii.cs
+++ Medium/45-jump-game-ii/45-jump-game-ii.cs
@@ -1,5 +1,27 @@
 public class Solution {
     public int Jump(int[] nums) {
+        var jumps = 0;
+        var minReachable = 0;
+        var maxReachable = 0;
+        
+        while(maxReachable < nums.Length - 1) {
+            var currentReachable = 0;
+            
+            for(int i = minReachable; i <= maxReachable; i++) {
+                currentReachable = Math.Max(currentReachable, nums[i] + i);    
+            }
+            
+            minReachable = maxReachable + 1;
+            maxReachable = currentReachable;
+            jumps++;
+        }
+        
+        return jumps;
+    }
+}
+
+public class SolutionDP {
+    public int Jump(int[] nums) {
         var solution = new int[nums.Length];
         Array.Fill(solution, Int32.MaxValue);
         
