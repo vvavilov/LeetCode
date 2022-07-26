a261540631cb0020b0388ab0c23df8513d3b32a0
vvavilov <vvavilov@users.noreply.github.com>
2022-07-26 18:43:18 +0400

    Time: 115 ms (56.35%), Space: 36.7 MB (43.21%) - LeetHub

--- Medium/198-house-robber/198-house-robber.cs
+++ Medium/198-house-robber/198-house-robber.cs
@@ -1,46 +1,23 @@
 public class Solution {
-//     1 2 3 4 5 6
-//
-        
-        //     6 -> 6
-    //     56 -> max(5,6);
-    //    456 -> max(sum(4,f(6), f(5))
-    //   3456 -> max(sum(3,f(56)), sum(4,f(6))
-    //  23456 -> max(sum(2, f(456)), sum(3, f(56)))
-    // 123456 -> max(sum(1, f(3456)), sum(2, f(456)),
-//         f(n) = {
-//                  6 -> n[6]
-//                  5 -> n[5]
-                    // 1-4 -> max(n[n] + n[n+2], n[n+1])
-//         
-            
-            
     public int Rob(int[] nums) {
-        
-        
-        var plan = MakePlan(nums, nums.Length - 1);
-        return Math.Max(plan.Rob, plan.DoNotRob);
-        
-    }
-    
-    private State MakePlan(int[] nums, int houseNumber) {
-        if(houseNumber == 0) {
-            return new State {
-                Rob = nums[0],
-                DoNotRob = 0
-            };
+        if(nums.Length == 0) {
+            return 0;
         }
         
-        var previous = MakePlan(nums, houseNumber - 1);
-        return new State {
-            Rob = previous.DoNotRob + nums[houseNumber],
-            DoNotRob = Math.Max(previous.DoNotRob, previous.Rob)
-        };
+        if(nums.Length == 1) {
+            return nums[0];
+        }
+        
+        var prevPrev = nums[0];
+        var prev = Math.Max(nums[0], nums[1]);
+        
+        foreach(var x in nums.Skip(2)) {
+            var bestSoFar = Math.Max(prevPrev + x, prev);
+            prevPrev = prev;
+            prev = bestSoFar;
+        }
+        
+        return Math.Max(prevPrev, prev);
         
     }
-}
-
-public class State {
-    public int Rob;
-    public int DoNotRob;
 }
