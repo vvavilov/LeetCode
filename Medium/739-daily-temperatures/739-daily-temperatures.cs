d4eff7b8174f19aa6f89c202b98255319c6d79ba
vvavilov <vvavilov@users.noreply.github.com>
2022-07-26 14:07:46 +0400

    Time: 458 ms (60.41%), Space: 49.7 MB (68.14%) - LeetHub

--- Medium/739-daily-temperatures/739-daily-temperatures.cs
+++ Medium/739-daily-temperatures/739-daily-temperatures.cs
@@ -1,5 +1,31 @@
 public class Solution {
     public int[] DailyTemperatures(int[] tmps) {
+        Stack<(int value, int index)> pending = new();    
+        int[] result = new int[tmps.Length];
+        
+        for(int i = 0; i < tmps.Length; i++) {
+            var cur = tmps[i];
+            
+            while(pending.Count > 0 && pending.Peek().value < cur) {
+                var prev = pending.Pop();
+                result[prev.index] = i - prev.index;
+            }
+            
+            pending.Push((cur, i));
+        }
+        
+        while(pending.Count > 0) {
+            var prev = pending.Pop();
+            result[prev.index] = 0;
+        }
+        
+        return result;
+        
+    }
+}
+
+public class SolutionHashTable {
+    public int[] DailyTemperatures(int[] tmps) {
         Dictionary<int, int> dict = new();
 
         var result = new int[tmps.Length];
