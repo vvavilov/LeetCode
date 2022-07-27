8502471885a183c1170aacc2f058b091135848b5
vvavilov <vvavilov@users.noreply.github.com>
2022-07-27 22:18:20 +0400

    Time: 3030 ms (5.19%), Space: 159.6 MB (15.56%) - LeetHub

--- Medium/1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cs
+++ Medium/1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cs
@@ -1,12 +1,13 @@
 public class Solution {
-    private Dictionary<(int left, int right, int i), int> dict = new();
+    private Dictionary<(int left, int i), int> dict = new();
 
     public int MaximumScore(int[] nums, int[] mult) {
-        return CalculateMaximum(nums, mult, 0, nums.Length - 1, 0);
+        return CalculateMaximum(nums, mult, 0, 0);
     }
     
-    private int CalculateMaximum(int[] nums, int[] mult, int left, int right, int i) {
-        var key = (left, right, i);
+    private int CalculateMaximum(int[] nums, int[] mult, int left, int i) {
+        var right = nums.Length - 1 + left - i;
+        var key = (left, i);
         
         if(dict.ContainsKey(key)) {
             return dict[key];
@@ -16,8 +17,8 @@ public class Solution {
             return 0;
         }
         
-        var leftMax = nums[left] * mult[i] + CalculateMaximum(nums, mult, left + 1, right, i + 1);
-        var rightMax = nums[right] * mult[i] + CalculateMaximum(nums, mult, left, right - 1, i + 1);
+        var leftMax = nums[left] * mult[i] + CalculateMaximum(nums, mult, left + 1, i + 1);
+        var rightMax = nums[right] * mult[i] + CalculateMaximum(nums, mult, left, i + 1);
         dict[key] = Math.Max(leftMax, rightMax);
         return dict[key];
     }
