c1b8e5e8155a5b03db59d645ba81b8a98658bddf
vvavilov <vvavilov@users.noreply.github.com>
2022-07-27 22:26:53 +0400

    Time: 2568 ms (25.93%), Space: 50 MB (94.07%) - LeetHub

--- Medium/1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cs
+++ Medium/1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cs
@@ -1,25 +1,30 @@
 public class Solution {
-    private Dictionary<(int left, int i), int> dict = new();
+    private int[][] cache;
 
     public int MaximumScore(int[] nums, int[] mult) {
+        cache = new int[mult.Length][];
+
+        for(int i = 0; i < mult.Length; i++) {
+            cache[i] = new int[mult.Length];
+        }
+        
         return CalculateMaximum(nums, mult, 0, 0);
     }
     
     private int CalculateMaximum(int[] nums, int[] mult, int left, int i) {
-        var right = nums.Length - 1 + left - i;
-        var key = (left, i);
-        
-        if(dict.ContainsKey(key)) {
-            return dict[key];
-        }
-        
         if(i == mult.Length) {
             return 0;
         }
         
+        var right = nums.Length - 1 + left - i;
+
+        if(cache[left][i] != 0) {
+            return cache[left][i];
+        }
+                
         var leftMax = nums[left] * mult[i] + CalculateMaximum(nums, mult, left + 1, i + 1);
         var rightMax = nums[right] * mult[i] + CalculateMaximum(nums, mult, left, i + 1);
-        dict[key] = Math.Max(leftMax, rightMax);
-        return dict[key];
+        cache[left][i] = Math.Max(leftMax, rightMax);
+        return cache[left][i];
     }
 }
