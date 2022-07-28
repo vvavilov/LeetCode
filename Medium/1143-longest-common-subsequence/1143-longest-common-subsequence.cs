afa13bdb7dd4ba93421ad82b00bae77fb53e62d5
vvavilov <vvavilov@users.noreply.github.com>
2022-07-28 10:34:53 +0400

    Time: 74 ms (91.78%), Space: 40.3 MB (16.71%) - LeetHub

--- Medium/1143-longest-common-subsequence/1143-longest-common-subsequence.cs
+++ Medium/1143-longest-common-subsequence/1143-longest-common-subsequence.cs
@@ -21,25 +21,4 @@ public class Solution {
         
         return hash[0][0];
     }
-    
-    private int TopDown(string t1, string t2, int pos1, int pos2) {
-        if(pos1 == t1.Length || pos2 == t2.Length) {
-            return 0;
-        }
-        
-        if(hash[pos1][pos2] != -1) {
-            return hash[pos1][pos2];
-        }
-        
-        if(t1[pos1] == t2[pos2]) {
-            return 1 + TopDown(t1, t2, pos1 + 1, pos2 + 1);
-        }
-        
-        hash[pos1][pos2] = Math.Max(
-            TopDown(t1, t2, pos1 + 1, pos2),
-            TopDown(t1, t2, pos1, pos2 + 1)
-        );
-        
-        return hash[pos1][pos2];
-    }
 }
