0c4b0020ce342bfcd69af7bd1b97d13f579cf35d
vvavilov <vvavilov@users.noreply.github.com>
2022-07-28 10:34:40 +0400

    Time: 80 ms (85.34%), Space: 40.5 MB (10.68%) - LeetHub

--- Medium/1143-longest-common-subsequence/1143-longest-common-subsequence.cs
+++ Medium/1143-longest-common-subsequence/1143-longest-common-subsequence.cs
@@ -1,15 +1,25 @@
 public class Solution {
     private int[][] hash;
     
-    public int LongestCommonSubsequence(string text1, string text2) {
-        hash = new int[text1.Length][];
+    public int LongestCommonSubsequence(string t1, string t2) {
+        hash = new int[t1.Length + 1][];
         
         for(int i = 0; i < hash.Length; i++) {
-            hash[i] = new int[text2.Length];
-            Array.Fill(hash[i], -1);
+            hash[i] = new int[t2.Length + 1];
         }
         
-        return TopDown(text1, text2, 0, 0);    
+        for(int i = t1.Length - 1; i >= 0; i--) {
+            for(int j = t2.Length - 1; j >= 0; j--) {
+                if(t1[i] == t2[j]) {
+                    hash[i][j] = hash[i+1][j+1] + 1;
+                    continue;
+                }
+                
+                hash[i][j] = Math.Max(hash[i][j+1], hash[i+1][j]);
+            }
+        }
+        
+        return hash[0][0];
     }
     
     private int TopDown(string t1, string t2, int pos1, int pos2) {
