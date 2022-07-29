c1f8ac0c64151695bc076116172d5ea38d3d3793
vvavilov <vvavilov@users.noreply.github.com>
2022-07-29 13:47:22 +0400

    Time: 175 ms (24.40%), Space: 40 MB (75.87%) - LeetHub

--- Medium/323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.cs
+++ Medium/323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.cs
@@ -1,5 +1,67 @@
 public class Solution {
     public int CountComponents(int n, int[][] edges) {
+        var roots = new int[n];
+        
+        for(int i = 0; i < roots.Length; i++) {
+            roots[i] = i;
+        }
+        
+        foreach(var edge in edges) {
+            Union(roots, edge[0], edge[1]);
+        }
+        
+        return SetsCount(roots);
+    }
+
+    
+    private int SetsCount(int[] roots) {
+        var count = 0;
+        
+        for(int i = 0; i < roots.Length; i++) {
+            if(roots[i] == i) {
+                count++;
+            } 
+        }
+        
+        return count;
+    }
+    
+    private int Root(int[] roots, int i) {
+        // Compression
+        while(roots[i] != i) {
+            i = roots[i];
+        }
+        
+        return i;
+    }
+    
+    public void Union(int[] roots, int i, int j) {
+        // Ranking
+        var leftRoot = Root(roots, j);
+        var rightRoot = Root(roots, i);
+        
+        if(leftRoot == rightRoot) {
+            return;
+        }
+        
+        roots[rightRoot] = leftRoot;
+    }
+    
+    private void Visit(int node, bool[] visited, List<int>[] graph) {
+        if(visited[node]) {
+            return;
+        }
+
+        visited[node] = true;
+        
+        foreach(var x in graph[node]) {
+            Visit(x, visited, graph);
+        }
+    }
+}
+
+public class SolutionOneArrayPass {
+    public int CountComponents(int n, int[][] edges) {
         var graph = BuildGraph(n, edges);
         var visited = new bool[n];
         var setsCount = 0;
