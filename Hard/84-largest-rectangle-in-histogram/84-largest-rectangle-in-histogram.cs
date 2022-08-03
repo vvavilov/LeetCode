c7d7147a8ac8d8e8de3e881d30142980f8a8dc2a
vvavilov <vvavilov@users.noreply.github.com>
2022-08-03 10:29:19 +0400

    Time: 225 ms (95.91%), Space: 48.9 MB (26.09%) - LeetHub

---
+++ Hard/84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cs
@@ -0,0 +1,42 @@
+public class Solution {
+    public int LargestRectangleArea(int[] heights) {
+        Stack<(int height, int count)> visited = new();
+        var max = 0;
+        
+        for(int i = 0; i < heights.Length; i++) {
+            var h = heights[i];
+            
+            if(visited.Count == 0 || visited.Peek().height < h) {
+                visited.Push((h, 1));
+                continue;
+            }
+            
+            if(visited.Peek().height == h) {
+                var prev = visited.Pop();
+                visited.Push((h, prev.count + 1));
+                continue;
+            }
+            
+            var accumulatedCount = 0;
+
+            while(visited.Count > 0 && visited.Peek().height >= h) {
+                var prev = visited.Pop();
+                accumulatedCount += prev.count;
+                max = Math.Max(max, accumulatedCount * prev.height);
+            }
+            
+            visited.Push((h, accumulatedCount + 1));
+        }
+        
+        var count = 0;
+
+        while(visited.Count > 0) {
+            // Optimize?
+            var item = visited.Pop();
+            count += item.count;
+            max = Math.Max(max, count * item.height);
+        }
+        
+        return max;
+    }
+}
