8c6bebeb4775e961d30840bc77db01af9408c1dd
vvavilov <vvavilov@users.noreply.github.com>
2022-08-04 15:55:42 +0400

    Time: 37 ms (77.78%), Space: 25.4 MB (11.11%) - LeetHub

---
+++ Medium/858-mirror-reflection/858-mirror-reflection.cs
@@ -0,0 +1,42 @@
+public class Solution {
+    public int MirrorReflection(int p, int q) {
+        double roomsVerticalCount = 0;
+        double roomsHorizontalCount = 1;
+        
+        // 30, 20
+        
+        while(roomsVerticalCount * p / ( roomsHorizontalCount  * q) != 1) {
+            roomsVerticalCount++;
+            
+            while(roomsVerticalCount * p > roomsHorizontalCount * q) {
+                roomsHorizontalCount++;
+            }
+            
+        }
+        
+        var verticalFlipsCount = roomsVerticalCount - 1;
+        var horizontalFlipsCount = roomsHorizontalCount - 1;
+        var verticalFlipped = verticalFlipsCount % 2 != 0;
+        var horizontalFlipped = horizontalFlipsCount % 2 != 0;
+        
+        Console.WriteLine(verticalFlipsCount);
+        Console.WriteLine(horizontalFlipsCount);
+
+        Console.WriteLine(verticalFlipped);
+        Console.WriteLine(horizontalFlipped);
+        
+        if(!verticalFlipped && !horizontalFlipped) {
+            return 1;
+        }
+        
+        if(verticalFlipped && !horizontalFlipped) {
+            return 0;
+        }
+        
+         if(!verticalFlipped && horizontalFlipped) {
+            return 2;
+        }
+        
+        return -1;
+    }
+}
