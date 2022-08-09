6de665271c604d3e56f019fa647a3c14972a2477
vvavilov <vvavilov@users.noreply.github.com>
2022-08-09 23:39:54 +0400

    Time: 434 ms (25.75%), Space: 46.3 MB (98.38%) - LeetHub

---
+++ Medium/15-3sum/15-3sum.cs
@@ -0,0 +1,39 @@
+public class Solution {
+    public IList<IList<int>> ThreeSum(int[] nums) {
+        Array.Sort(nums);
+
+        List<IList<int>> result = new();
+        
+        for(int i = 0; i < nums.Length; i++) {
+            if(nums[i] > 0) {
+                break;
+            }
+            
+            if(i > 0 && nums[i] == nums[i-1]) {
+                continue;
+            }
+        
+            var set = new HashSet<int>();
+
+            
+            for(int j = i + 1; j < nums.Length; j++) {                
+                var rest = 0 - nums[i] - nums[j];
+                
+                if(set.Contains(rest)) {
+                    result.Add(new List<int> { nums[i], nums[j], rest });
+                    
+                    while(j + 1 < nums.Length && nums[j+1] == nums[j]) {
+                        j++;
+                    }
+                    
+                }
+                
+                if(!set.Contains(nums[j])) {
+                    set.Add(nums[j]);
+                }
+            }
+        }
+        
+        return result;
+    }
+}
