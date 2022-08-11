d276b4b5fd47ccd8ecd6d8f912821668f5e895a0
vvavilov <vvavilov@users.noreply.github.com>
2022-08-11 23:45:51 +0400

    Time: 178 ms (6.10%), Space: 39.4 MB (56.10%) - LeetHub

---
+++ Medium/777-swap-adjacent-in-lr-string/777-swap-adjacent-in-lr-string.cs
@@ -0,0 +1,61 @@
+public class Solution {
+    public bool CanTransform(string start, string end) {
+        var startArray = start.ToCharArray();
+        var endArray = end.ToCharArray();
+        
+        if (start.Length != end.Length) {
+            return false;
+        }
+        
+        for(int i = 0; i < start.Length; i++) {
+            var curStart = startArray[i];
+            var curEnd = endArray[i];
+            
+            if(curEnd == 'R') {
+                if(curStart == 'R') {
+                    continue;
+                }
+                
+                return false;
+            }
+            
+            if(curEnd == 'X') {
+                var curPos = i;
+
+                while(curPos < startArray.Length && startArray[curPos] == 'R') {
+                    curPos++;
+                }
+                
+                if(curPos < startArray.Length &&  startArray[curPos] == 'X') {
+                    Swap(startArray, i, curPos);
+                    continue;
+                }
+                
+                return false;
+            }
+            
+            if(curEnd == 'L') {
+                var curPos = i;
+
+                while(curPos < startArray.Length && startArray[curPos] == 'X') {
+                    curPos++;
+                }
+                
+                if(curPos < startArray.Length && startArray[curPos] == 'L') {
+                    Swap(startArray, i, curPos);
+                    continue;
+                }
+                
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    private void Swap(char[] arr, int left, int right) {
+        var temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+    }
+}
