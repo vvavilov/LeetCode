cbbf9b5bab92caf3d691ec185e12e95b14803557
vvavilov <vvavilov@users.noreply.github.com>
2022-08-24 14:00:03 +0400

    Time: 218 ms (15.66%), Space: 42.2 MB (7.23%) - LeetHub

--- Medium/673-number-of-longest-increasing-subsequence/673-number-of-longest-increasing-subsequence.cs
+++ Medium/673-number-of-longest-increasing-subsequence/673-number-of-longest-increasing-subsequence.cs
@@ -1,103 +1,58 @@
 public class Solution {
-    public int FindNumberOfLIS(int[] nums) {
-        var mem = new (int, int)[nums.Length];
-        
-        mem[0] = (1, 1);
-        
-        for(int i = 1; i < nums.Length; i++) {
-            var maxLength = 1;
-            var maxCount = 1;
-            
-            for(int j = 0; j < i; j++) {
-                (var leftSize, var leftCount) = mem[j];
-                
-                if(nums[j] >= nums[i]) {
-                    continue;
-                }
-                
-                var length = leftSize + 1;
-                
-                if(length > maxLength) {
-                    maxLength = length;
-                    maxCount = leftCount;
-                } else if (length == maxLength) {
-                    maxCount += leftCount;
-                }
-            }
-            
-            mem[i] = (maxLength, maxCount);
-
-        }
-                    
-        return GetMaxLengthCount(mem);
-    }
+    Dictionary<int, (int Length, int Count)> dp = new();
     
-    private int GetMaxLengthCount((int, int)[] results) {
-        var maxLength = 0;
-        var maxCount = 0;
-        
-        for(int i = 0; i < results.Length; i++) {
-            (var length, var count) = results[i];
-            
-            if(length > maxLength) {
-                maxLength = length;
-                maxCount = count;
-            } else if(maxLength == length) {
-                maxCount += count;
-            }
-        }
-        
-        return maxCount;
-        
-    }
-}
-
-public class SolutionTopDown {
     public int FindNumberOfLIS(int[] nums) {
-        var maxLength = 0;
         var maxCount = 0;
+        var maxLength = 0;
         
-        var mem = new Dictionary<int, (int, int)>();
         for(int i = 0; i < nums.Length; i++) {
-            (var length, var count) = FindDP(nums, i, mem);
+            var result = TopDown(i, nums);
             
-            if(length > maxLength) {
-                maxLength = length;
-                maxCount = count;
-            } else if(length == maxLength) {
-                maxCount += count;
+            if(result.Length > maxLength) {
+                maxLength = result.Length;
+                maxCount = result.Count;
+                continue;
+            }
+            
+            if(result.Length == maxLength) {
+                maxCount += result.Count;
             }
         }
         
         return maxCount;
     }
     
-    private (int length, int count) FindDP(int[] nums, int pos, Dictionary<int, (int, int)> mem) {
-        if(mem.ContainsKey(pos)) {
-            return mem[pos];
+    private (int Length, int Count) TopDown(int pos, int[] nums) {
+        if(dp.ContainsKey(pos)) {
+            return dp[pos];
         }
         
-        var maxLength = 1;
-        var count = 1;
+        dp[pos] = (1, 1);
         
         for(int i = pos + 1; i < nums.Length; i++) {
             if(nums[i] <= nums[pos]) {
                 continue;
             }
-
-            (var childLength, var childCount) = FindDP(nums, i, mem);
-            var length = childLength + 1;
             
-            if(length > maxLength) {
-                maxLength = length;
-                count = childCount;
-            } else if(length == maxLength) {
-                count += childCount;
-            }
+            var child = TopDown(i, nums);
+            
+            var length = 1 + child.Length;
+            UpdateMax(length, child.Count, pos, dp);
         }
         
-        mem[pos] = (maxLength, count);
-        return (maxLength, count);
-
+        return dp[pos];
+    }
+    
+       private void UpdateMax(int length, int count, int pos, Dictionary<int, (int Length, int Count)> dp) {
+        var cur = dp[pos];
+        
+        if(cur.Length == length) {
+            dp[pos] = (length, count + dp[pos].Count);
+            return;
+        }
+        
+        if(cur.Length < length) {
+            dp[pos] = (length, count);
+        }
     }
 }
