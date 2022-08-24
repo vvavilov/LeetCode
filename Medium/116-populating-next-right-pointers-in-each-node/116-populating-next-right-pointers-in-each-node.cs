914b9b2fc99d6c839fab01a89af0dcf8c1e68f34
vvavilov <vvavilov@users.noreply.github.com>
2022-08-24 19:22:09 +0400

    Time: 159 ms (28.05%), Space: 39.2 MB (97.68%) - LeetHub

--- Medium/116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.cs
+++ Medium/116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.cs
@@ -23,6 +23,38 @@ public class Node {
 
 public class Solution {
     public Node Connect(Node root) {
+        if(root == null) {
+            return null;
+        }
+
+        Queue<Node> queue = new();
+        queue.Enqueue(root);
+        
+        while(queue.Count > 0) {
+            var count = queue.Count;
+            Node next = null;
+
+            while(count-- > 0) {
+                var node = queue.Dequeue();
+                node.next = next;
+                next = node;
+                
+                if(node.right != null) {
+                    queue.Enqueue(node.right);
+                }
+                
+                if(node.left != null) {
+                    queue.Enqueue(node.left);
+                }
+            }
+        }
+        
+        return root; 
+    }
+}
+
+public class SolutionDfs {
+    public Node Connect(Node root) {
         Connect(root, null);
         return root;
     }
