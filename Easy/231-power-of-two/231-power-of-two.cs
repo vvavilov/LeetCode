d523beb93c7194f25688dbee3fb7362d37d68a9a
vvavilov <vvavilov@users.noreply.github.com>
2022-08-24 10:33:04 +0400

    Time: 55 ms (39.97%), Space: 27.1 MB (54.09%) - LeetHub

---
+++ Easy/231-power-of-two/231-power-of-two.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public bool IsPowerOfTwo(int n) {
+        if(n == 0) {
+            return false;
+        }
+        
+        while (n % 2 == 0) {
+            n = n / 2;
+        }
+        
+        return n == 1;
+    }
+}
