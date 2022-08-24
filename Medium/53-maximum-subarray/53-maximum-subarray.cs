da1f052a5a9d8688762a4730dd625a4386bf33bf
vvavilov <vvavilov@users.noreply.github.com>
2022-08-24 20:01:03 +0400

    Time: 448 ms (5.03%), Space: 48.8 MB (64.72%) - LeetHub

---
+++ Medium/53-maximum-subarray/53-maximum-subarray.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public int MaxSubArray(int[] nums) {
+        if(nums.Length == 0) {
+            return 0;
+        }
+        
+        var maxVal = nums[0];
+        var sum = nums[0];
+        
+        for(int i = 1; i < nums.Length; i++) {
+            if (sum < 0) {
+                maxVal = Math.Max(maxVal, sum);
+                sum = nums[i];
+            } else {
+                sum += nums[i];
+            }
+            
+            maxVal = Math.Max(sum, maxVal);
+        }
+        
+        return maxVal;
+    }
+}
