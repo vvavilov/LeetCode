26a2d7c2345eba3b43920285404cb194e64e16b9
vvavilov <vvavilov@users.noreply.github.com>
2022-08-25 10:33:46 +0400

    Time: 175 ms (70.88%), Space: 45.6 MB (38.97%) - LeetHub

--- Medium/416-partition-equal-subset-sum/416-partition-equal-subset-sum.cs
+++ Medium/416-partition-equal-subset-sum/416-partition-equal-subset-sum.cs
@@ -6,6 +6,44 @@ public class Solution {
             return false;
         }
         
+        var target = sum / 2;
+        var dp = new bool[nums.Length][];
+        dp[0] = new bool[target + 1];
+        dp[0][0] = true;
+        
+        if(nums[0] <= target) {
+            dp[0][nums[0]] = true;
+        }
+            
+        for(int i = 1; i < dp.Length; i++) {
+            dp[i] = new bool[target + 1];
+            
+            for(int j = 0; j <= target; j++) {
+                var prev = dp[i - 1][j];
+                
+                if(prev) {
+                    dp[i][j] = true;
+                    
+                    if(j + nums[i] <= target) {
+                        dp[i][j + nums[i]] = true;
+                    }
+                    
+                }
+            }
+        }
+        
+        return dp.Any(x => x[target]);
+    }
+}
+
+public class SolutionTopDown {
+    public bool CanPartition(int[] nums) {
+        var sum = nums.Sum();
+        
+        if(sum % 2 != 0) {
+            return false;
+        }
+        
         var dp = new bool?[nums.Length][];
     
         for(int i = 0; i < dp.Length; i++) {
