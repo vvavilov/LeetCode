abd0778c6a55343d788ee3abbccb2605554f341f
vvavilov <vvavilov@users.noreply.github.com>
2022-08-25 10:51:21 +0400

    Time: 157 ms (80.73%), Space: 45.5 MB (39.83%) - LeetHub

--- Medium/416-partition-equal-subset-sum/416-partition-equal-subset-sum.cs
+++ Medium/416-partition-equal-subset-sum/416-partition-equal-subset-sum.cs
@@ -7,32 +7,32 @@ public class Solution {
         }
         
         var target = sum / 2;
-        var dp = new bool[nums.Length][];
+        
+        // dummy first row
+        var dp = new bool[nums.Length + 1][];
         dp[0] = new bool[target + 1];
         dp[0][0] = true;
         
-        if(nums[0] <= target) {
-            dp[0][nums[0]] = true;
-        }
-            
         for(int i = 1; i < dp.Length; i++) {
+            Console.WriteLine("HERE " + i);
+
             dp[i] = new bool[target + 1];
             
             for(int j = 0; j <= target; j++) {
                 var prev = dp[i - 1][j];
-                
+
                 if(prev) {
                     dp[i][j] = true;
                     
-                    if(j + nums[i] <= target) {
-                        dp[i][j + nums[i]] = true;
+                    if(j + nums[i - 1] <= target) {
+                        dp[i][j + nums[i - 1]] = true;
                     }
                     
                 }
             }
         }
-        
-        return dp.Any(x => x[target]);
+                
+        return dp[dp.Length - 1][target];
     }
 }
 
@@ -54,15 +54,12 @@ public class SolutionTopDown {
     }
     
     private bool DP(int[] nums, int pos, int target, bool?[][] dp) {
-        if(target < 0) {
-            return false;
-        }
-        
         if(target == 0) {
             return true;
         }
         
-        if(pos == nums.Length) {
+        
+        if(target < 0 || pos == nums.Length) {
             return false;
         }
         
