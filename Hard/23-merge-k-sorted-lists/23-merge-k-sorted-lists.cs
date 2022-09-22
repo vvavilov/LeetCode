882912e58278b355f46de58cf5186829635d2591
vvavilov <vvavilov@users.noreply.github.com>
2022-09-22 12:59:15 +0400

    Time: 93 ms (99.29%), Space: 43 MB (11.39%) - LeetHub

--- Hard/23-merge-k-sorted-lists/23-merge-k-sorted-lists.cs
+++ Hard/23-merge-k-sorted-lists/23-merge-k-sorted-lists.cs
@@ -9,7 +9,48 @@
  *     }
  * }
  */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
 public class Solution {
+    public ListNode MergeKLists(ListNode[] lists) {
+        var pq = new PriorityQueue<int, int>();
+
+        for(int i = 0; i < lists.Length; i++) {
+            if(lists[i] == null) {
+                continue;
+            }
+            pq.Enqueue(i, lists[i].val);
+        }
+
+        var dummy = new ListNode();
+        var cur = dummy;
+
+        while(pq.Count > 0) {
+            var listN = pq.Dequeue();
+            cur.next = new ListNode(lists[listN].val);
+            lists[listN] = lists[listN].next;
+            cur = cur.next;
+
+            if(lists[listN] != null) {
+                pq.Enqueue(listN, lists[listN].val);
+            }
+        }
+
+        return dummy.next;
+    }
+}
+
+public class SolutionBruteForse {
     public ListNode MergeKLists(ListNode[] lists) {
         if(lists.Length == 0) {
             return null;            
@@ -50,3 +91,4 @@ public class Solution {
         return result.next;
     }
 }
+
