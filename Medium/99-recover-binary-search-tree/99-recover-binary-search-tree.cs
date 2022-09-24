fa1012f056cc0eef1a8f5716e9f46b27eb333df8
vvavilov <vvavilov@users.noreply.github.com>
2022-09-24 18:00:10 +0400

    Time: 180 ms (29.24%), Space: 39.6 MB (46.20%) - LeetHub

---
+++ Medium/99-recover-binary-search-tree/99-recover-binary-search-tree.cs
@@ -0,0 +1,63 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+/*
+            1
+        2       6
+      4   3   5    7    
+
+
+
+*/
+public class Solution {
+    private bool leftFound = false;
+    private TreeNode left;
+    private TreeNode right;
+    
+    public void RecoverTree(TreeNode root) {
+        InorderTraversal(root, null);
+        Swap(left, right);
+    }
+   
+    // l=3 r=2
+    private TreeNode InorderTraversal(TreeNode node, TreeNode prev) {
+        if(node == null) {
+            return prev;
+        }
+        
+        
+        prev = InorderTraversal(node.left, prev);
+        
+        if(prev != null && prev.val > node.val) {
+            if(!leftFound) {
+                left = prev;
+                right = node;
+                leftFound = true;
+            } else {
+                right = node;
+            }
+        }
+
+        
+        prev = InorderTraversal(node.right, node);
+        return prev;
+        
+    }
+    
+    private void Swap(TreeNode left, TreeNode right) {
+        var temp = left.val;
+        left.val = right.val;
+        right.val = temp;
+    }
+}
