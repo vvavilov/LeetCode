968a5bf9bc0a215c35d1ead1efca910eb7372729
vvavilov <vvavilov@users.noreply.github.com>
2022-09-27 14:43:45 +0400

    Time: 160 ms (91.38%), Space: 47.5 MB (17.24%) - LeetHub

--- Medium/907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.cs
+++ Medium/907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.cs
@@ -5,38 +5,39 @@ public class Solution {
         Stack<int> stack = new();
         double sum = 0;
         var itemRightBorder = 0;
-
-        for(int i = 0; i < arr.Length; i++) {
-            while(stack.Count > 0 && arr[stack.Peek()] >= arr[i]) {
+        var source = new List<int>(arr);
+        source.Add(Int32.MinValue);
+        
+        for(int i = 0; i < source.Count; i++) {
+            while(stack.Count > 0 && arr[stack.Peek()] >= source[i]) {
                 var itemPos = stack.Pop();
                 var itemLeftBorder = stack.Count > 0 ? stack.Peek() + 1 : 0;
-                itemRightBorder = i - 1;
-                
-                var subarraysCount = (0d + itemPos - itemLeftBorder) * (itemRightBorder - itemPos + 1) % mod + itemRightBorder - itemPos + 1;
-                
-                sum += subarraysCount * arr[itemPos] % mod;
+                var subarraysCount = (0d + itemPos - itemLeftBorder) * (i - itemPos) % mod + i - itemPos;
+                sum += subarraysCount * source[itemPos] % mod;
                 sum %= mod;
             }
             
             stack.Push(i);            
         }
         
-        if(stack.Count == 0) {
-            return (int)sum;
-        }
+        return (int)sum;
         
-        itemRightBorder = stack.Peek();
+//         if(stack.Count == 0) {
+//             return (int)sum;
+//         }
         
-        while(stack.Count > 0) {
-            var itemPos = stack.Pop();
-            var itemLeftBorder = stack.Count > 0 ? stack.Peek() + 1 : 0;
+//         itemRightBorder = stack.Peek();
+        
+//         while(stack.Count > 0) {
+//             var itemPos = stack.Pop();
+//             var itemLeftBorder = stack.Count > 0 ? stack.Peek() + 1 : 0;
             
-            var subarraysCount = (0d + itemPos - itemLeftBorder) * (itemRightBorder - itemPos + 1) % mod + itemRightBorder - itemPos + 1;
+//             var subarraysCount = (0d + itemPos - itemLeftBorder) * (itemRightBorder - itemPos + 1) % mod + itemRightBorder - itemPos + 1;
             
-            sum += subarraysCount * arr[itemPos] % mod;
-            sum %= mod;
-        }
+//             sum += subarraysCount * arr[itemPos] % mod;
+//             sum %= mod;
+//         }
         
-        return  (int)sum;
+//         return  (int)sum;
     }
 }
