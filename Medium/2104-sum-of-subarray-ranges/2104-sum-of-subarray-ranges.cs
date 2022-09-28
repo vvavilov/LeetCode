0ac53292db62f1a4f0069b82bd7f83cd686145e4
vvavilov <vvavilov@users.noreply.github.com>
2022-09-28 15:00:16 +0400

    Time: 160 ms (56.47%), Space: 39.3 MB (31.90%) - LeetHub

---
+++ Medium/2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cs
@@ -0,0 +1,56 @@
+public class Solution {
+    public long SubArrayRanges(int[] nums) {
+        return MaximumSum(nums) - MinimumSum(nums);
+    }
+
+    private long MinimumSum(int[] nums) {
+        Stack<int> increasing = new();
+        long total = 0;
+        var listOfNums = new List<int>(nums);
+        listOfNums.Add(Int32.MinValue);
+
+        for(int i = 0; i < listOfNums.Count; i++) {
+            var cur = listOfNums[i];
+
+            while(increasing.Count > 0 && listOfNums[increasing.Peek()] > cur) {
+                var itemPos = increasing.Pop();
+                var leftBorder = increasing.Count > 0 ? increasing.Peek() + 1 : 0;
+                var subarraysCount = 0L + (itemPos - leftBorder) * (i - itemPos) + i - itemPos;
+                total += subarraysCount * listOfNums[itemPos];
+            }
+
+            increasing.Push(i);
+        }
+
+        
+        Console.WriteLine("Min");
+
+        Console.WriteLine(total);
+
+        return total;
+
+    }
+
+    private long MaximumSum(int[] nums) {
+        Stack<int> decreasing = new();
+        long total = 0;
+        var listOfNums = new List<int>(nums);
+        listOfNums.Add(Int32.MaxValue);
+
+        for(int i = 0; i < listOfNums.Count; i++) {
+            var cur = listOfNums[i];
+
+            while(decreasing.Count > 0 && listOfNums[decreasing.Peek()] < cur) {
+                var itemPos = decreasing.Pop();
+                var leftBorder = decreasing.Count > 0 ? decreasing.Peek() + 1 : 0;
+                var subarraysCount = 0L + (itemPos - leftBorder) * (i - itemPos) + i - itemPos;
+                total += subarraysCount * listOfNums[itemPos];
+            }
+            
+            decreasing.Push(i);
+        }
+
+        return total;
+
+    }
+}
