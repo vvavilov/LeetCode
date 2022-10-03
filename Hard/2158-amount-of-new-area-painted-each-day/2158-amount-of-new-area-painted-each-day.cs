714254c021d6345ebdfd75a2bc4575b575de32d3
vvavilov <vvavilov@users.noreply.github.com>
2022-10-03 14:20:32 +0400

    Time: 1394 ms (41.79%), Space: 117.4 MB (5.97%) - LeetHub

--- Hard/2158-amount-of-new-area-painted-each-day/2158-amount-of-new-area-painted-each-day.cs
+++ Hard/2158-amount-of-new-area-painted-each-day/2158-amount-of-new-area-painted-each-day.cs
@@ -1,5 +1,48 @@
 public class Solution {
     public int[] AmountPainted(int[][] paint) {
+        var days = new SortedSet<int>();
+        var result = new int[paint.Length];
+        
+        var events = new List<(int pos, bool isStart, int index)>();
+
+        var lastPos = 0;
+        
+        for(int i = 0; i < paint.Length; i++) {
+            events.Add((paint[i][0], true, i));
+            events.Add((paint[i][1], false, i));
+            lastPos = Math.Max(lastPos, paint[i][1]);
+        }
+        
+        events = events.OrderBy(x => x.pos).ToList();
+
+        var eventPos = 0;
+        
+        for(int i = 0; i <= lastPos; i++) {
+            Console.WriteLine("position " + i);
+            // var hasEvent = eventsPos < events.Length && events[eventPos].pos == i;
+            while(eventPos < events.Count && events[eventPos].pos == i) {
+                var paintEvent = events[eventPos];
+
+                if(paintEvent.isStart) {
+                    days.Add(paintEvent.index);
+                } else {
+                    days.Remove(paintEvent.index);
+                }
+
+                eventPos++;
+            }
+            
+            if(days.Count > 0) {
+                result[days.First()]++;    
+            }  
+        }
+        
+        return result;
+    }
+}
+
+public class SolutionQuadraticTime {
+    public int[] AmountPainted(int[][] paint) {
         var result = new List<int>();
         var paintedIntervals = new List<(int start, int end)>();
         
