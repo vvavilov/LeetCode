5fa30bf27d0a4353c000423cad97c47c3966af45
vvavilov <vvavilov@users.noreply.github.com>
2022-10-08 16:46:21 +0400

    Time: 144 ms (83.33%), Space: 36.9 MB (50.00%) - LeetHub

--- Hard/726-number-of-atoms/726-number-of-atoms.cs
+++ Hard/726-number-of-atoms/726-number-of-atoms.cs
@@ -1,63 +1,57 @@
 public class Solution {
+    
     public string CountOfAtoms(string formula) {
-        SortedDictionary<string, int> byCount = new();
+        
         Stack<int> multipliers = new();
-        multipliers.Push(1);
-        
-        var i = formula.Length - 1;
-        
-        while(i >= 0) {
-            var symbol = formula[i];
-            var elementName = "";
-            var elementCount = 0;
-            
-            if(symbol == '(') {
-                multipliers.Pop();
-                i = i - 1;
-                continue;
-            }
-            
-            if(symbol == ')') {
-                multipliers.Push(multipliers.Peek());
-                i = i - 1;
-                continue;
-            }
-            
-            if(Char.IsDigit(symbol)) {
-                (var token, var startingPos) = ParseDigit(formula, i);
-                
-                if(token is ClosingBracket) {
-                    var closingToken = token as ClosingBracket;
-                    multipliers.Push(closingToken.Count * multipliers.Peek());
-                    i = startingPos - 1;
-                    continue;
-                } else {
-                    var element = token as Element;
-                    elementName = element.Name;
-                    elementCount = element.Count;
-                }
-                
-                i = startingPos - 1;
+        SortedDictionary<string, int> byCount = new();
 
-            } else {
-                if(Char.IsUpper(symbol)) {
-                    elementName = symbol.ToString();
-                    elementCount = 1;
-                    i = i - 1;
-                }
+        var currentNumber = 0;
+        var numberMultiplier = 1;
         
-                if(Char.IsLower(symbol)) {
-                    elementName = formula.Substring(i - 1, 2);
-                    elementCount = 1;
-                    i = i - 2;
-                }
+        multipliers.Push(1);
+
+        for(int i = formula.Length - 1; i >= 0; i--) {
+            var element = formula[i];
+
+            if(Char.IsDigit(element)) {
+                currentNumber = currentNumber + Int32.Parse(element.ToString()) * numberMultiplier;
+                numberMultiplier *= 10;
+                continue;
+            }  
+
+            if(element == '(') {
+                multipliers.Pop();
             }
-            
-            byCount.TryGetValue(elementName, out var count);
-            byCount[elementName] = count + multipliers.Peek() * elementCount;
+
+            if(element == ')') {
+                multipliers.Push(multipliers.Peek() * (currentNumber == 0 ? 1 : currentNumber));
+            }
+
+            if(Char.IsUpper(element)) {
+                Increase(byCount, multipliers.Peek() * (currentNumber == 0 ? 1 : currentNumber), element.ToString());    
+            }
+
+            if(Char.IsLower(element)) {
+                var elementName = formula.Substring(i - 1, 2);
+                Increase(byCount, multipliers.Peek() * (currentNumber == 0 ? 1 : currentNumber), elementName);
+                i--;
+            }
+
+            currentNumber = 0;
+            numberMultiplier = 1;
         }
 
-        var result = new StringBuilder();
+
+        return Format(byCount);
+    }
+    
+    private void Increase(SortedDictionary<string, int> byCount, int count, string name) {
+        byCount.TryGetValue(name, out var current);
+        byCount[name] = count + current;
+    }
+    
+    private string Format(SortedDictionary<string, int> byCount) {
+        StringBuilder result = new();
         
         foreach(var x in byCount) {
             result.Append(x.Key);
@@ -65,52 +59,8 @@ public class Solution {
             if(x.Value > 1) {
                 result.Append(x.Value);
             }
-
         }
-
+        
         return result.ToString();
     }
-    
-    private (IToken token, int startPos) ParseDigit(string formula, int pos) {        
-        var multiplier = 1;
-        var count = 0;
-        
-        while(true) {
-            if(Char.IsDigit(formula[pos])) {
-                count = Int32.Parse(formula[pos].ToString()) * multiplier + count;
-                multiplier *= 10;
-                pos--;
-                continue;
-            }
-            
-            if (formula[pos] == ')')  {
-                return (new ClosingBracket { Count = count }, pos);   
-            }
-            
-            if (Char.IsUpper(formula[pos])) {
-                return (new Element { Count = count, Name = formula[pos].ToString() }, pos);
-            }
-            
-            if(Char.IsLower(formula[pos])) {
-                return (new Element { Count = count, Name = formula.Substring(pos - 1, 2) }, pos - 1);
-            }            
-        }
-        
-        return (null, 0);
-
-    }
 }
-
-public interface IToken {
-    
-}
-
-public class ClosingBracket : IToken {
-    public int Count { get; set; }
-}
-
-public class Element: IToken {
-    public int Count { get; set; }
-    public string Name { get; set; }
-}
-
