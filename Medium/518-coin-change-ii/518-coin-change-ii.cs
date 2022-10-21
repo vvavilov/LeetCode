3294e1894bc6d5bc18fddb4f5966816f097d0426
vvavilov <vvavilov@users.noreply.github.com>
2022-10-21 13:11:45 +0400

    Time: 458 ms (14.29%), Space: 96.5 MB (10.08%) - LeetHub

--- Medium/518-coin-change-ii/518-coin-change-ii.cs
+++ Medium/518-coin-change-ii/518-coin-change-ii.cs
@@ -1,34 +1,30 @@
+
+
 public class Solution {
     int[] coins;
 
     public int Change(int amount, int[] coins) {
         this.coins = coins;
-        Array.Sort(this.coins);
         return DP(amount, 0, new Dictionary<(int, int), int>());
     }
     
-    public int DP(int amount, int minCoinPos, Dictionary<(int, int), int> dp) {
-        
+    public int DP(int amount, int coinPos, Dictionary<(int, int), int> dp) {
         if(amount == 0) {
             return 1;
         }
         
-        var state = (amount, minCoinPos);
+        if(amount < 0 || coinPos == coins.Length) {
+            return 0;
+        }
+        
+        var state = (amount, coinPos);
         
         if(dp.ContainsKey(state)) {
             return dp[state];
         }
     
-        var count = 0;
-        
-        for(int i = minCoinPos; i < coins.Length; i++) {
-            if(coins[i] > amount) {
-                break;
-            }
-            
-            count += DP(amount - coins[i], i, dp);
-        }
-        
+        var count = DP(amount - coins[coinPos], coinPos, dp) + DP(amount, coinPos + 1, dp);
+
         dp[state] = count; 
         return count;
         
