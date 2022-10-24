36d0142d94494b166e373040ffd960217b1e3b5d
vvavilov <vvavilov@users.noreply.github.com>
2022-10-24 12:53:18 +0400

    Time: 200 ms (68.94%), Space: 45.6 MB (41.85%) - LeetHub

--- Medium/416-partition-equal-subset-sum/416-partition-equal-subset-sum.cs
+++ Medium/416-partition-equal-subset-sum/416-partition-equal-subset-sum.cs
@@ -1,73 +1,40 @@
 public class Solution {
     public bool CanPartition(int[] nums) {
-        var sum = nums.Sum();
-        
-        if(sum % 2 != 0) {
+        if(nums.Sum() % 2 != 0) {
             return false;
         }
         
-        var target = sum / 2;
+        var dp = new bool[nums.Length][];
+        var target = nums.Sum() / 2;
         
-        // dummy first row
-        var dp = new bool[nums.Length + 1][];
-        dp[0] = new bool[target + 1];
-        dp[0][0] = true;
+        for(int i = 0; i < dp.Length; i++) {
+            dp[i] = new bool[target + 1];
+        }
+        
+        if(nums[0] <= target) {
+            dp[0][nums[0]] = true;
+        }
+        
         
         for(int i = 1; i < dp.Length; i++) {
-            dp[i] = new bool[target + 1];
+            if(nums[i] <= target) {
+                dp[i][nums[i]] = true;
+
+            }
             
             for(int j = 0; j <= target; j++) {
-                var prev = dp[i - 1][j];
+                if(dp[i - 1][j]) {
+                    dp[i][j] = dp[i - 1][j];
+                    var sum = j + nums[i];
 
-                if(prev) {
-                    dp[i][j] = true;
-                    
-                    if(j + nums[i - 1] <= target) {
-                        dp[i][j + nums[i - 1]] = true;
+                    if(sum <= target) {
+                        dp[i][sum] = true; 
                     }
-                    
                 }
+                
             }
         }
-                
-        return dp[dp.Length - 1][target];
-    }
-}
-
-public class SolutionTopDown {
-    public bool CanPartition(int[] nums) {
-        var sum = nums.Sum();
         
-        if(sum % 2 != 0) {
-            return false;
-        }
-        
-        var dp = new bool?[nums.Length][];
-    
-        for(int i = 0; i < dp.Length; i++) {
-            dp[i] = new bool?[sum / 2 + 1];
-        }
-        
-        return DP(nums, 0, sum / 2, dp);
-    }
-    
-    private bool DP(int[] nums, int pos, int target, bool?[][] dp) {
-        if(target == 0) {
-            return true;
-        }
-        
-        
-        if(target < 0 || pos == nums.Length) {
-            return false;
-        }
-        
-        if(dp[pos][target] is bool calculated) {
-            return calculated;
-        }
-        
-        var val = nums[pos];
-        
-        dp[pos][target] = DP(nums, pos + 1, target - val, dp) || DP(nums, pos + 1, target, dp);
-        return (bool)dp[pos][target];
+        return dp.Any(x => x[target]);
     }
 }
