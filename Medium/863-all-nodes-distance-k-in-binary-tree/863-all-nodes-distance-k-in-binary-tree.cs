5e9d448a8a2d201fdd2e69611c6a1975f9ad3f47
vvavilov <vvavilov@users.noreply.github.com>
2022-10-30 23:06:07 +0400

    Time: 262 ms (20.52%), Space: 41.7 MB (87.34%) - LeetHub

--- Medium/863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cs
+++ Medium/863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cs
@@ -9,139 +9,47 @@
  */
 public class Solution {
     public IList<int> DistanceK(TreeNode root, TreeNode target, int k) {
-        List<int> solution = new();
-        DistanceToChildTarget(root, target, solution, k);
-        return solution;
-    }
-    
-    private int DistanceToChildTarget(TreeNode node, TreeNode target, IList<int> solution, int distance) {
-        if(node == null) {
-            return -1;
-        }
-        
-        if(node == target) {
-            MarkNodesAtDistance(node, distance, solution);
-            return 0;
-        }
-     
-        var leftDistance = DistanceToChildTarget(node.left, target, solution, distance) + 1;
-        
-        if(leftDistance > 0) {
-            var restDistance = distance - leftDistance;
-            
-            if(restDistance == 0) {
-                solution.Add(node.val);
-            } else {
-                MarkNodesAtDistance(node.right, restDistance - 1, solution);
-            }
-
-            return leftDistance;
-            
-        }
-        
-        var rightDistance = DistanceToChildTarget(node.right, target, solution, distance) + 1;
-
-        if(rightDistance > 0) {
-            var restDistance = distance - rightDistance;
-            
-            if(restDistance == 0) {
-                solution.Add(node.val);
-            } else {
-                MarkNodesAtDistance(node.left, restDistance - 1, solution);
-            }
-
-            return rightDistance;
-        }
-        
-        return -1;
-    }
-    
-    private void MarkNodesAtDistance(TreeNode node, int distance, IList<int> solution) {
-        if(node == null || distance < 0) {
-            return;
-        }
-        
-        if(distance == 0) {
-            solution.Add(node.val);
-            return;
-        }
-        
-        MarkNodesAtDistance(node.left, distance - 1, solution);
-        MarkNodesAtDistance(node.right, distance - 1, solution);
-
-    }
-    
-    
-}
-
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     public int val;
- *     public TreeNode left;
- *     public TreeNode right;
- *     public TreeNode(int x) { val = x; }
- * }
- */
-public class SolutionWithParents {
-    public IList<int> DistanceK(TreeNode root, TreeNode target, int k) {
-        Dictionary<int, TreeNode> parents = new();
-        FillParents(root, parents);
-        
-        var distance = 0;
-        var queue = new Queue<TreeNode>();
-        queue.Enqueue(target);
-        
+        Dictionary<TreeNode, TreeNode> parents = new();
+        parents[root] = null;
+        BuildParents(root, parents);
         List<int> result = new();
-        HashSet<int> visited = new();
-        
-        while(queue.Count > 0) {
-            var count = queue.Count;
-            
-            for(int i = 0; i < count; i++) {
-                var node = queue.Dequeue();
-                
-                if(visited.Contains(node.val)) {
-                    continue;
-                }
-                
-                if(k == distance) {
-                    result.Add(node.val);
-                    continue;
-                }
-                
-                visited.Add(node.val);
-                parents.TryGetValue(node.val, out var parent);
-                
-                if(parent != null) {
-                    queue.Enqueue(parent);
-                }
-                
-                if(node.left != null) {
-                    queue.Enqueue(node.left);
-                }
-                
-                if(node.right != null) {
-                    queue.Enqueue(node.right);
-                }
-            }
-            
-            distance++;
-        }
-        
+        FindAt(target, k, result, new HashSet<int>(), parents);
         return result;
-        
     }
     
-    private void FillParents(TreeNode node, Dictionary<int, TreeNode> parents) {
+    private void FindAt(TreeNode node, int k, List<int> result, HashSet<int> visited, Dictionary<TreeNode, TreeNode> parents) {
+        if(node == null) {
+            return;
+        }
+        
+        if(visited.Contains(node.val)) {
+            return;
+        }
+        
+        visited.Add(node.val);
+        
+        if(k == 0) {
+            result.Add(node.val);
+        }
+        
+        FindAt(node.left, k - 1, result, visited, parents);
+        FindAt(node.right, k - 1, result, visited, parents);
+        FindAt(parents[node], k - 1, result, visited, parents);
+
+
+    }   
+
+    private void BuildParents(TreeNode node, Dictionary<TreeNode, TreeNode> parents) {
         if(node.left != null) {
-            parents[node.left.val] = node;
-            FillParents(node.left, parents);
+            parents[node.left] = node;
+            BuildParents(node.left, parents);
         }
         
         if(node.right != null) {
-            parents[node.right.val] = node;
-            FillParents(node.right, parents);
+            parents[node.right] = node;
+            BuildParents(node.right, parents);
+
         }
-    } 
+    }
+    
 }
