097b6e32cebaa30b233e64aa8b9fb3608249e6ff
vvavilov <vvavilov@users.noreply.github.com>
2022-10-30 22:16:14 +0400

    Time: 202 ms (50.28%), Space: 42 MB (97.06%) - LeetHub

--- Medium/207-course-schedule/207-course-schedule.cs
+++ Medium/207-course-schedule/207-course-schedule.cs
@@ -1,106 +1,55 @@
 public class Solution {
     public bool CanFinish(int numCourses, int[][] prerequisites) {
         var graph = BuildGraph(numCourses, prerequisites);
-        var processed = new HashSet<int>();
-        
-        for(int i = 0; i < numCourses; i++) {
-            if(HasCycle(graph, i, new HashSet<int>(), processed)) {
-                return false;
-            }
-        }
-        
-        return true;
-        
-        
+        return TopologicalSort(graph);
     }
     
-    private bool HasCycle(List<List<int>> graph, int node, HashSet<int> processing, HashSet<int> processed) {
-        if(processed.Contains(node)) {
-            return false;
-        }
-        
-        if(processing.Contains(node)) {
-            return true;
-        }
-        
-        processing.Add(node);
-        foreach(var neigh in graph[node]) {
-            if(HasCycle(graph, neigh, processing, processed)) {
-                return true;
-            }
-        }
-        
-        processed.Add(node);        
-        return false;
-    }
-    
-    private List<List<int>> BuildGraph(int n, int[][] prereq) {
-        List<List<int>> graph = new();
+    private List<int>[] BuildGraph(int n, int[][] prereq) {
+        var graph = new List<int>[n];
         
         for(int i = 0; i < n; i++) {
-            graph.Add(new List<int>());
+            graph[i] = new List<int>();
         }
         
-        for(int i = 0; i < prereq.Length; i++) {
-            var dependsOn = prereq[i][1];
-            var dependy = prereq[i][0];
-            graph[dependsOn].Add(dependy);
+        foreach(var x in prereq) {
+            graph[x[1]].Add(x[0]);
         }
         
         return graph;
     }
-}
-
-public class SolutionTopological {
-    public bool CanFinish(int numCourses, int[][] prerequisites) {
-        var graph = BuildGraph(numCourses, prerequisites);
-        
-        var inboundCount = new int[numCourses];
+    
+    private bool TopologicalSort(List<int>[] graph) {
+        var inboundCount = new int[graph.Length];
         
         foreach(var node in graph) {
-            foreach(var child in node) {
-                inboundCount[child]++;
+            foreach(var neigh in node) {
+                inboundCount[neigh]++;
             }
         }
         
-        Queue<int> queue = new();
-        int processedCount = 0;
+        Queue<int> nodes = new();
+        var processedNodes = 0;
         
         for(int i = 0; i < inboundCount.Length; i++) {
             if(inboundCount[i] == 0) {
-                queue.Enqueue(i);
+                nodes.Enqueue(i);
             }
         }
         
-        while(queue.Count > 0) {
-            var node = queue.Dequeue();
-            processedCount++;
+        while(nodes.Count > 0) {
+            var node = nodes.Dequeue();
+            processedNodes++;
             
-            foreach(var child in graph[node]) {
-                inboundCount[child]--;
+            foreach(var x in graph[node]) {
+                inboundCount[x]--;
                 
-                if(inboundCount[child] == 0) {
-                    queue.Enqueue(child);
+                if(inboundCount[x] == 0) {
+                    nodes.Enqueue(x);
                 }
             }
+            
         }
         
-        return numCourses == processedCount;
-    }
-    
-    private List<List<int>> BuildGraph(int n, int[][] prereq) {
-        List<List<int>> graph = new();
-        
-        for(int i = 0; i < n; i++) {
-            graph.Add(new List<int>());
-        }
-        
-        for(int i = 0; i < prereq.Length; i++) {
-            var dependsOn = prereq[i][1];
-            var dependy = prereq[i][0];
-            graph[dependsOn].Add(dependy);
-        }
-        
-        return graph;
+        return processedNodes == graph.Length;
     }
 }
