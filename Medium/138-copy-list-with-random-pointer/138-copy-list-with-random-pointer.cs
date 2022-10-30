30898141b134dd5f6d6c3b7f8f2c3306073b7dc1
vvavilov <vvavilov@users.noreply.github.com>
2022-10-30 22:27:07 +0400

    Time: 164 ms (31.88%), Space: 38 MB (45.13%) - LeetHub

--- Medium/138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cs
+++ Medium/138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cs
@@ -15,51 +15,24 @@ public class Node {
 
 public class Solution {
     public Node CopyRandomList(Node head) {
-        if(head == null) {
+        Dictionary<Node, Node> copies = new();
+        return Copy(head, copies);
+    }
+    
+    private Node Copy(Node node, Dictionary<Node, Node> copies) {
+        if(node == null) {
             return null;
         }
+
+        if(copies.ContainsKey(node)) {
+            return copies[node];
+        }
         
-        Dictionary<Node, Node> visited = new();
-        Queue<(Node node, Node parent, bool isRandom)> toVisit = new();
-        var headCopy = new Node(head.val);
-        visited.Add(head, headCopy);
-
-        if(head.random != null) {
-            toVisit.Enqueue((head.random, headCopy, true));
-        }
-
-        if(head.next != null) {
-            toVisit.Enqueue((head.next, headCopy, false));
-        }
-
-        while(toVisit.Count > 0) {
-            var toProcessItems = toVisit.Dequeue();
-            
-            var copy = visited.ContainsKey(toProcessItems.node)
-                ? visited[toProcessItems.node]
-                : new Node(toProcessItems.node.val);
-
-            if(toProcessItems.isRandom) {
-                toProcessItems.parent.random = copy;
-            } else {
-                toProcessItems.parent.next = copy;
-            }
-
-            if(visited.ContainsKey(toProcessItems.node)) {
-                continue;
-            }
-
-            visited.Add(toProcessItems.node, copy);
-
-            if(toProcessItems.node.random != null) {
-                toVisit.Enqueue((toProcessItems.node.random, copy, true));
-            }
-
-            if(toProcessItems.node.next != null) {
-                toVisit.Enqueue((toProcessItems.node.next, copy, false));
-            }
-        }
-
-        return headCopy;
+        var copy = new Node(node.val);
+        copies[node] = copy;
+        
+        copy.next = Copy(node.next, copies);
+        copy.random = Copy(node.random, copies);
+        return copy;
     }
 }
