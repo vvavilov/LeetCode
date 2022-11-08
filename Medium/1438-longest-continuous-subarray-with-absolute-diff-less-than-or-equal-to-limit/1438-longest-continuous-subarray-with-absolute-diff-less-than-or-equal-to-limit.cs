250ff17dfaad0ce8a9db62e2cadacc86552c4da2
vvavilov <vvavilov@users.noreply.github.com>
2022-11-08 22:00:19 +0400

    Time: 420 ms (43.64%), Space: 52.6 MB (34.54%) - LeetHub

---
+++ Medium/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cs
@@ -0,0 +1,43 @@
+
+public class Solution {
+    public int LongestSubarray(int[] nums, int limit) {
+        var left = 0;
+        var right = 0;
+        var length = 0;
+        
+        var max = new LinkedList<int>();
+        var min = new LinkedList<int>();
+        
+        while(right < nums.Length) {
+            while(max.Count > 0 && nums[max.Last.Value] < nums[right]) {
+                max.RemoveLast();
+            }
+            
+            max.AddLast(right);
+            
+            while(min.Count > 0 && nums[min.Last.Value] > nums[right]) {
+                min.RemoveLast();
+            }
+            
+            min.AddLast(right);
+            
+            while(nums[max.First.Value] - nums[min.First.Value] > limit) {
+                if(max.First.Value == left) {
+                    max.RemoveFirst();
+                } else if(min.First.Value == left) {
+                    min.RemoveFirst();
+                }
+                
+                left++;
+
+            }
+            
+
+            length = Math.Max(length, right - left + 1);
+            right++;
+        }
+        
+        return length;
+
+    }
+}
