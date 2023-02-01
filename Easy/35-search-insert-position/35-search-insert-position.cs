34bd0bb8154bc9f09bf8be85a6725d4d8aaba411
vvavilov <vvavilov@users.noreply.github.com>
2023-02-01 19:51:36 +0100

    Time: 84 ms (89.93%), Space: 39.3 MB (37.36%) - LeetHub

--- Easy/35-search-insert-position/35-search-insert-position.cs
+++ Easy/35-search-insert-position/35-search-insert-position.cs
@@ -1,20 +1,24 @@
 public class Solution {
     public int SearchInsert(int[] nums, int target) {
-        var start = 0;
-        var end = nums.Length - 1;
-
-        while (start <= end) {
-            var mid = (end - start) /2 + start;
+        var left = 0;
+        var right = nums.Length - 1;
+        
+        while(left <= right) {
+            var mid = (right - left) / 2 + left;
+            
             if(nums[mid] == target) {
                 return mid;
             }
-            if(nums[mid] > target) {
-                end = mid - 1;
+            
+            if(nums[mid] < target) {
+                left = mid + 1;
             } else {
-                start = mid + 1;
+                right = mid - 1;
             }
         }
-
-        return start;
+        
+         return left;
     }
+    
+   
 }
