b374ef5140c43f30c2cdaafd3acdf75072ff50be
vvavilov <vvavilov@users.noreply.github.com>
2023-02-01 19:29:45 +0100

    Time: 98 ms (39.29%), Space: 39.6 MB (56.64%) - LeetHub

--- Easy/21-merge-two-sorted-lists/21-merge-two-sorted-lists.cs
+++ Easy/21-merge-two-sorted-lists/21-merge-two-sorted-lists.cs
@@ -8,31 +8,32 @@
  *         this.next = next;
  *     }
  * }
- 
- 1 - 3 - 5 - 7
- 2 - 3 - 6
  */
 public class Solution {
-    
-    
     public ListNode MergeTwoLists(ListNode list1, ListNode list2) {
-        if(list1 == null) {
-            return list2;
+        var dummyResult = new ListNode();
+        var currentResult = dummyResult;
+        
+        while(list1 != null && list2 != null) {
+            if(list1.val < list2.val) {
+                currentResult.next = list1;
+                list1 = list1.next;
+            } else {
+                currentResult.next = list2;
+                list2 = list2.next;
+            }
+            
+            currentResult = currentResult.next;
         }
         
-        if(list2 == null) {
-            return list1;
+        var rest = list1 ?? list2;
+        
+        while(rest != null) {
+            currentResult.next = rest;
+            rest = rest.next;
+            currentResult = currentResult.next;
         }
         
-        if(list1.val < list2.val) {
-            var node = list1;
-            node.next = MergeTwoLists(list1.next, list2);
-            return node;
-        } else {
-            var node = list2;
-            node.next = MergeTwoLists(list1, list2.next);
-            return node;
-        }
-        
+        return dummyResult.next;
     }
 }
