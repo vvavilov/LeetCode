b1e0e7918abc05803ec954bd736ee215bbd9c790
vvavilov <vvavilov@users.noreply.github.com>
2023-02-03 21:02:18 +0100

    Time: 93 ms (53.59%), Space: 38.8 MB (56.55%) - LeetHub

--- Medium/33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cs
+++ Medium/33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cs
@@ -1,49 +1,43 @@
 public class Solution {
     public int Search(int[] nums, int target) {
-        if(nums.Length == 0) {
-            return -1;
-        }
-      
+        // 0 1 2 3 4 5 6 7
+        // 7 0 1 2 3 4 5 6
+        // (5 6 7) (0 1 2 3)
+        //    t       h
+        
+        // [1]
+        // [1 1 1]
+        
+        // handle not rotated
+        // handle single
+        // handle duplicates
+        
         var left = 0;
         var right = nums.Length - 1;
-        var first = nums[0];
-        
-        while(left < right) {
-            var mid = (right - left) / 2 + left;
-            if(nums[mid] >= first) {
-                left = mid + 1;
-            } else {
-                right = mid;
-            }
-        }
-        
-        var splitPoint = left;
-        
-        left = 0;
-        right = nums.Length - 1;
-       
-        if(target >= first) {
-            right = splitPoint;
-        } else {
-            left = splitPoint;
-        }
-        
-        return BinarySearch(nums, target, left, right);
-    }
-    
-    private int BinarySearch(int[] nums, int target, int left, int right) {
+        var tailStartsWith = nums[0];
+        var targetIsInTail = target >= nums[0];
         
         while(left <= right) {
             var mid = (right - left) / 2 + left;
-            
+        
             if(nums[mid] == target) {
                 return mid;
             }
             
+            var isTail = nums[mid] >= tailStartsWith;
+            
             if(target > nums[mid]) {
-                left = mid + 1;
+                if(isTail || !targetIsInTail) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
             } else {
-                right = mid - 1 ;
+                if(!isTail || targetIsInTail) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
             }
         }
         
