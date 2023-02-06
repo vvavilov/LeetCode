40916626ae0cc547145da04cf2d602d576cd8f3c
vvavilov <vvavilov@users.noreply.github.com>
2023-02-06 19:26:13 +0100

    Time: 98 ms (83.18%), Space: 40.8 MB (16.95%) - LeetHub

--- Medium/74-search-a-2d-matrix/74-search-a-2d-matrix.cs
+++ Medium/74-search-a-2d-matrix/74-search-a-2d-matrix.cs
@@ -1,24 +1,18 @@
 public class Solution {
     public bool SearchMatrix(int[][] matrix, int target) {
-        if(matrix.Length == 0) {
-            return false;
-        }
-        
-        var rowsN = matrix.Length;
-        var colsN = matrix[0].Length;
+        var total = matrix.Length * matrix[0].Length;
         var left = 0;
-        var right = rowsN * colsN - 1;
+        var right = total - 1;
         
         while(left <= right) {
             var mid = (right - left) / 2 + left;
-            var midRow = mid / colsN;
-            var midCol = mid % colsN;
+            (var y, var x) = FromId(matrix, mid);
             
-            if(matrix[midRow][midCol] == target) {
+            if(matrix[y][x] == target) {
                 return true;
             }
             
-            if(matrix[midRow][midCol] < target) {
+            if(matrix[y][x] < target) {
                 left = mid + 1;
             } else {
                 right = mid - 1;
@@ -26,5 +20,11 @@ public class Solution {
         }
         
         return false;
+    }
+    
+    private (int y, int x) FromId(int[][] matrix, int id) {
+        var y = id / matrix[0].Length;
+        var x = id - y * matrix[0].Length;
+        return (y, x);
     }
 }
