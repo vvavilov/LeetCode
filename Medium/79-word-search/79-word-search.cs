346912fc7e693f79db0274cb0d12c82c52617c9c
vvavilov <vvavilov@users.noreply.github.com>
2023-02-06 20:35:59 +0100

    Time: 1152 ms (31.63%), Space: 40.8 MB (44.79%) - LeetHub

--- Medium/79-word-search/79-word-search.cs
+++ Medium/79-word-search/79-word-search.cs
@@ -1,4 +1,8 @@
 public class Solution {
+    private HashSet<(int y, int x)> visited = new();
+    private string word;
+    private char[][] board;
+    
     private (int y, int x)[] directions = new (int y, int x)[] {
         (0, 1),
         (1, 0),
@@ -7,49 +11,57 @@ public class Solution {
     };
     
     public bool Exist(char[][] board, string word) {
-       for(int i = 0; i < board.Length; i++) {
-          for(int j = 0; j < board[0].Length; j++) {
-              if(ExistsStartingFrom(i, j, board, word, 0, new HashSet<(int y, int x)>())) {
-                  return true;
-              }
-          } 
-       }
-
+        this.word = word;
+        this.board = board;
+        
+        for(int i = 0; i < board.Length; i++) {
+            for(int j = 0; j < board[0].Length; j++) {
+                if(Backtrack(board, (i, j), 0)) {
+                    return true;
+                }
+            }
+        }
+        
         return false;
     }
     
-    private bool ExistsStartingFrom(int y, int x, char[][] board, string word, int pos, HashSet<(int y, int x)> used) {
-        if(pos == word.Length) {
+    private bool Backtrack(char[][] board, (int y, int x) cell, int pos) {
+        if (board[cell.y][cell.x] != word[pos]) {
+            return false;
+        }
+        
+        visited.Add(cell);
+        
+        if(visited.Count == word.Length) {
             return true;
         }
-
-        if(used.Contains((y, x))) {
-            return false;
-        }        
         
-        if(!IsInBoundaries(board, y, x) || board[y][x] != word[pos]) {
-            return false;
-        }
-        
-        used.Add((y, x));
-
         foreach(var dir in directions) {
-            if(ExistsStartingFrom(y + dir.y, x + dir.x, board, word, pos + 1, used)) {
-                
+            (int y, int x) neigh = (cell.y + dir.y, cell.x + dir.x);
+            
+            if(!IsInsideBoard(board, neigh) || visited.Contains(neigh)) {
+                continue;
+            }
+            
+            if(Backtrack(board, neigh, pos + 1)) {
                 return true;
             }
         }
-
-        used.Remove((y, x));
+        
+        visited.Remove(cell);
         return false;
     }
     
-    private bool IsInBoundaries(char[][] board, int y, int x) {
-        return y >= 0
-            && y < board.Length
-            && x >= 0
-            && x < board[0].Length;
+    private bool IsInsideBoard(char[][] board, (int y, int x) cell) {
+        if(cell.y < 0 || cell.x < 0) {
+            return false;
+        } 
+        
+        if(cell.y >= board.Length || cell.x >= board[0].Length) {
+            return false;
+        }
+        
+        return true;
     }
-    
 
 }
