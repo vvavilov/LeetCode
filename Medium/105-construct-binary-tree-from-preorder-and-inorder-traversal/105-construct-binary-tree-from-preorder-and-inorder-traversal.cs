c237bb3efc683d4d15fca72ed6b648f2a5ea7ccf
vvavilov <vvavilov@users.noreply.github.com>
2023-02-07 21:52:58 +0100

    Time: 103 ms (46.06%), Space: 41.2 MB (23.97%) - LeetHub

--- Medium/105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cs
+++ Medium/105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cs
@@ -12,54 +12,50 @@
  * }
  */
 public class Solution {
-    private ParentSource parents;
-    private NodePositionSource positionSource;
+    int[] preorder;
+    int[] inorder;
+    int preorderPos = 0;
     
     public TreeNode BuildTree(int[] preorder, int[] inorder) {
-        parents = new ParentSource(preorder);
-        positionSource = new NodePositionSource(inorder);
-        return BuildSubtree(0, inorder.Length - 1);
+        this.preorder = preorder;
+        this.inorder = inorder;
+        return RecursiveImpl(0, preorder.Length - 1);
     }
     
-    private TreeNode BuildSubtree(int leftPos, int rightPos) {
-        if(rightPos < leftPos) {
+    private TreeNode RecursiveImpl(int leftInorder, int rightInorder) {
+        if(rightInorder < leftInorder) {
             return null;
         }
-        
-        var parent = parents.Next;
-        var posOfParent = positionSource.GetPosition(parent);
-        
-        var parentNode = new TreeNode(parent);
-        
-        parentNode.left = BuildSubtree(leftPos, posOfParent - 1);
-        parentNode.right = BuildSubtree(posOfParent + 1, rightPos);
-        
-        return parentNode;
-    }
-}
 
-public class NodePositionSource {
-    Dictionary<int, int> dict = new();
-    
-    public NodePositionSource(int[] nodes) {
-        for(int i = 0; i < nodes.Length; i++) {
-            dict.Add(nodes[i], i);
+        if(leftInorder == rightInorder) {
+            preorderPos++;
+            return new TreeNode(inorder[leftInorder]);
         }
+        
+        var parent = new TreeNode(preorder[preorderPos]);
+        var inorderParentPos = Array.FindIndex(inorder, x => x == parent.val);
+        preorderPos++;
+        parent.left = RecursiveImpl(leftInorder, inorderParentPos - 1);
+        parent.right = RecursiveImpl(inorderParentPos + 1, rightInorder);
+        return parent;
     }
-    
-    public int GetPosition(int nodeValue) {
-        return dict[nodeValue];
-    }
-}
+        
+/*
 
-public class ParentSource {
-    private int pos;
-    private int[] parents;
-    
-    public ParentSource(int[] parents) {
-        this.parents = parents;
-        pos = 0;
-    }
-    
-    public int Next => parents[pos++];
+                    1
+            2               3
+    4            5      6        7
+ 8     9    10     11  12      13
+
+
+preorder 1 2 4 9 5 10 11 3 6 12 7 13
+inorder  (4 9) 2 (10 5 11)) 1 (12 6 3 13 7)
+
+
+
+
+
+*/
+        // preorder: 3 9 20 15 7
+        // inorder: 9 3 15 20 7
 }
